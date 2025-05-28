0712484c9e01c1f61e35b3674b25acd7aec96dae
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-05-28 22:06:20 +0300

    js_training

--- text.js
+++ text.js
@@ -25,18 +25,37 @@ console.log(typeof status); // виведе "object"
 
 //TODO     Явне перетворення (Explicit Conversion):
 
-//? До числа (Number):
-//* Number("123") + // 123
-//* +"123"; // 123 (унарний плюс)
-//* parseInt("123px"); // 123
-//* parseFloat("123.45"); // 123.45
-//* Number("abc") → NaN
+//?   До числа (Number):
+//*     Number("123") + // 123
+//*     +"123"; // 123 (унарний плюс)
+//*     parseInt("123px"); // 123
+//*     parseFloat("123.45"); // 123.45
+//*     Number("abc") → NaN
 
-//? До рядка (String):
-//* String(123)    // "123"
-//* 123 + ""       // "123"
-//* true.toString() // "true"
+//?   До рядка (String):
+//*     String(123)    // "123"
+//*     123 + ""       // "123"
+//*     true.toString() // "true"
 
-//?  До булевого (Boolean):
+//?   До булевого (Boolean):
+//*    Boolean(0)       // false
+//*    Boolean("hello") // true
+//*    Boolean("")      // false
+//*    Boolean(null)    // false
+
+//?   Falsy" значення:
+//*    false
+//*    0
+//*    "" (порожній рядок)
+//*    null
+//*    undefined
+//*    NaN
 
 //TODO    Неявне перетворення (Implicit Coercion):
+
+//?    "5" + 1; // "51" (1 перетворюється в рядок)
+//?    "5" - 1; // 4   ("5" перетворюється в число)
+//?    true + 1; // 2   (true → 1)
+//?    false + "1"; // "false1" (false → "false")
+//?    null + 1; // 1
+//?    undefined + 1; // NaN
