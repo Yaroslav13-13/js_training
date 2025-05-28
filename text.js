1faf1fca5f40b0721b50540ae9b9c96ba088ee76
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-05-28 21:47:51 +0300

    js_training

---
+++ text.js
@@ -0,0 +1,20 @@
+//!        Оператор typeof
+const quantity = 17;
+console.log(typeof quantity); // виведе "number"
+const message = "JavaScript is awesome!";
+console.log(typeof message); // виведе "string"
+const isSidebarOpen = false;
+console.log(typeof isSidebarOpen); // виведе "boolean"
+let username;
+console.log(typeof username); // виведе "undefined"
+let status = null;
+console.log(typeof status); // виведе "object"
+_____________________________________________________________;
+//!        Комбіновані оператори
+//* Додавання: +=. Приклад: x += y еквівалентно x = x + y
+//* Віднімання: -=. Приклад: x -= y еквівалентно x = x - y
+//* Множення: *=. Приклад: x *= y еквівалентно x = x * y
+//* Ділення: /=. Приклад: x /= y еквівалентно x = x / y
+//* Остача від ділення: %=. Приклад: x %= y еквівалентно x = x % y
+_______________________________________________________________;
+//!       Перетворення типів: рядки
