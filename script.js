fbe527420eccb039d872a452b60a911f32c4d93b
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-05-28 21:08:50 +0300

    js_training

--- script.js
+++ script.js
@@ -1198,4 +1198,23 @@
 
 //?__________________________________________________________?\\
 
-//!========= Перебір об'єкта Цикл for...in ===================!\\
+//!================= Перебір об'єкта =================!\\
+//TODO -------------- Цикл for...in ----------------!\\
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+
+const keys = [];
+const values = [];
+
+for (const key in apartment) {
+  keys.push(key);
+  values.push(apartment[key]);
+}
+console.log(keys);
+console.log(values);
+
+//TODO --------------- Метод Object.keys() ----------------!\\
