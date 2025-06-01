dc34cfa59df9b891047ecfb81929f01894f894d6
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-02 00:09:29 +0300

    js_training

--- java_script/lesson.js
+++ java_script/lesson.js
@@ -34,12 +34,57 @@
 // console.log(obj.numbers[3]);
 //TODO_______________________________________________________________________
 
-const a = { x: 1, y: 2 };
-const b = a;
+// TODO      Об'єкти: типи даних за посиланням, масиви іфункції - це обєкти!
 
-a.X = 100;
+//* const a = { x: 1, y: 2 };
+//* const b = a;
 
-console.log(a);
-console.log(b);
+//* a.x = 100;
+//* b.x = 101;
+//* a.y = 10;
+//* a.f = 66;
+//* b.f = 606;
+
+//* console.log(a);
+//* console.log(b);
 
 //TODO_______________________________________________________________________
+
+// TODO      Об'єкти: короткі властивості, обчислювальні властивості!
+
+//?          короткі властивості
+
+//* const userName = "Yaroslav";
+//* const age = 25;
+//* const obj = {
+//*   userName,
+//*   age,
+//* };
+//* console.log(obj);
+
+//* function foo(name, age) {
+//*   return {
+//*     name,
+//*     age,
+//*   };
+//* }
+//* console.log(foo("Yaroslav", 37));
+
+//?          обчислювальні властивості
+
+//* const inputName = "color";
+//* const obj = {
+//*   [inputName]: "yellow",
+//* };
+
+//* console.log(obj);
+
+//TODO_______________________________________________________________________
+
+// TODO      Об'єкти: перебір об'єкта (цикл for...in; метод Object.keys; метод Object.value)
+
+//?          цикл for...in
+
+//?          метод Object.keys
+
+//?          метод Object.value
