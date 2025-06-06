f1406ffd3a0c67c0661ab48cfe2645b99f497316
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-06 11:52:32 +0300

    js_training

---
+++ java_script/JS lesson outline.js
@@ -0,0 +1,18 @@
+//! Модуль 5. Перебираючі методи масивів
+//  TODO Колбек-функції
+
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// const result = makePizza();
+// const pointer = makePizza;
+
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
