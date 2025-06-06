f6cb7b7dd17489f91bf7a8c618dca32dd291d5b2
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-06 11:58:01 +0300

    js_training

--- java_script/JS lesson outline.js
+++ java_script/JS lesson outline.js
@@ -16,3 +16,47 @@
 // function makeMessage(pizzaName, callback) {
 //   return callback(pizzaName);
 // }
+
+//TODO Інлайн-колбеки
+
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}`);
+// });
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//   console.log(`Eating pizza ${pizzaName}`);
+// });
+
+//TODO Метод forEach(callback)
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach(function (order) {
+//     totalPrice += order;
+//   });
+
+//   return totalPrice;
+// }
+
+//?   Функція filterArray(numbers, value) приймає першим параметром масив чисел numbers
+//?    і повертає новий масив,в якому будуть тільки ті елементи оригінального масиву,
+//?    які більші за значення другого параметра числа value.
+//?   Якщо таких значень не буде знайдено, функція повертає порожній масив.
+//?   Виконай рефакторинг функції таким чином, щоб замість циклу for,
+//?    вона використовувала метод forEach.
+
+//*   function filterArray(numbers, value) {
+//*     const result = [];
+//*     numbers.forEach(function (item) {
+//*       if (item > value) {
+//*         result.push(item);
+//*       }
+//*     });
+//*     return result;
+//*   }
+//*   console.log(filterArray([1, 2, 3, 4, 5], 3));
+//*   console.log(filterArray([1, 2, 3, 4, 5], 4));
