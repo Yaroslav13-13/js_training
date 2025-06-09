c41fd7613b7c6c758e421ccd076ac188aa801540
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-09 23:02:57 +0300

    js_training

--- java_script/tasks.js
+++ java_script/tasks.js
@@ -304,16 +304,25 @@
 // console.log(totalAveragePlaytimePerGame);
 //?______________________________________________________________________
 
-const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
-const authors = [
-  "Tanith Lee",
-  "Bernard Cornwell",
-  "Robert Sheckley",
-  "Fyodor Dostoevsky",
-];
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+// ];
 
-const ascendingReleaseDates = releaseDates.toSorted();
-const alphabeticalAuthors = authors.toSorted();
+// const ascendingReleaseDates = releaseDates.toSorted();
+// const alphabeticalAuthors = authors.toSorted();
+// console.log(ascendingReleaseDates);
+// console.log(alphabeticalAuthors);
+//?______________________________________________________________________
+
+const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+
+const ascendingReleaseDates = releaseDates.toSorted((a, b) => a - b);
 console.log(ascendingReleaseDates);
-console.log(alphabeticalAuthors);
+
+const descendingReleaseDates = releaseDates.toSorted((a, b) => b - a);
+console.log(descendingReleaseDates);
 //?______________________________________________________________________
