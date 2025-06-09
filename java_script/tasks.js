85674427595777004546dac50c4edaf39eb4d984
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-10 00:23:47 +0300

    js_training

--- java_script/tasks.js
+++ java_script/tasks.js
@@ -318,11 +318,43 @@
 // console.log(alphabeticalAuthors);
 //?______________________________________________________________________
 
-const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
 
-const ascendingReleaseDates = releaseDates.toSorted((a, b) => a - b);
-console.log(ascendingReleaseDates);
+// const ascendingReleaseDates = releaseDates.toSorted((a, b) => a - b);
+// console.log(ascendingReleaseDates);
 
-const descendingReleaseDates = releaseDates.toSorted((a, b) => b - a);
-console.log(descendingReleaseDates);
+// const descendingReleaseDates = releaseDates.toSorted((a, b) => b - a);
+// console.log(descendingReleaseDates);
 //?______________________________________________________________________
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  {
+    title: "The Dreams in the Witch House",
+    author: "Howard Lovecraft",
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
+
+const names = books
+  .filter((book) => book.rating > MIN_BOOK_RATING)
+  .map((book) => book.author)
+  .toSorted((a, b) => a.localeCompare(b));
+
+console.table(names);
