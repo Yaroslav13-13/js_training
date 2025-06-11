d809366b0ee79c89d18dfdc98748432ac74bf1f8
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-11 21:27:49 +0300

    js_training

--- java_script/script.js
+++ java_script/script.js
@@ -1205,3 +1205,56 @@
 // console.log(values);
 
 //TODO --------------- Метод Object.keys() ----------------!\\
+
+//!         Урок 1. Модуль 6. ООП. Класи
+
+// const user = {
+//   userName: "Alice",
+//   showThis() {
+//     console.log("this", this);
+//   },
+// };
+
+// function foo(callback) {
+//   console.log(callback);
+//   callback();
+// }
+
+// foo(user.showThis);
+
+const timePlayers = {
+  players: [
+    {
+      id: "player-1",
+      name: "Mango",
+      timePlayed: 310,
+      points: 54,
+      online: true,
+    },
+    {
+      id: "player-2",
+      name: "Poly",
+      timePlayed: 470,
+      points: 92,
+      online: false,
+    },
+    { id: "player-3", name: "Ajax", timePlayed: 180, points: 48, online: true },
+    {
+      id: "player-4",
+      name: "Kiwi",
+      timePlayed: 250,
+      points: 61,
+      online: false,
+    },
+    { id: "player-5", name: "Luna", timePlayed: 375, points: 80, online: true },
+  ],
+  calcTimePlayers(playersName) {
+    const player = this.players.find((item) => item.name === playersName);
+    if (!player) {
+      return `${playersName} not found`;
+    }
+    return player.timePlayed / player.points;
+  },
+};
+
+console.log(timePlayers.calcTimePlayers("Ajax"));
