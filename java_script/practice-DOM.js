701631a7eff1189c7fc050bca6b733c0e274a36d
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-07-20 22:06:49 +0300

    js_training

--- java_script/practice-DOM.js
+++ java_script/practice-DOM.js
@@ -116,87 +116,107 @@
 //* });
 //! ____________________________________________________________________
 
-// const title = document.querySelector(".title");
-// const inputTitle = document.querySelector(".input-title");
-// const btnTitle = document.querySelector(".btn-title");
-// const inputColor = document.querySelector(".input-color");
-// const btnColor = document.querySelector(".btn-color");
+const title = document.querySelector(".title");
+const inputTitle = document.querySelector(".input-title");
+const btnTitle = document.querySelector(".btn-title");
+const inputColor = document.querySelector(".input-color");
+const btnColor = document.querySelector(".btn-color");
 
-// btnTitle.addEventListener("click", () => {
-//   if (inputTitle.value.trim() !== "") {
-//     title.textContent = inputTitle.value;
-//   } else {
-//     alert("Введіть текст");
-//   }
-// });
-
-// btnColor.addEventListener("click", () => {
-//   if (inputColor.value.trim() !== "") {
-//     title.style.color = inputColor.value;
-//   } else {
-//     alert("Введіть колір");
-//   }
-// });
-
-// inputTitle.addEventListener("input", () => {
-//   title.textContent = inputTitle.value;
-// });
-
-// inputColor.addEventListener("input", () => {
-//   title.style.color = inputColor.value;
-// });
-
-// const fruits = document.querySelector("#fruitList");
-// const inputFruits = document.querySelector(".input");
-
-// fruits.addEventListener("click", (event) => {
-//   if (event.target.tagName === "LI") {
-//     inputFruits.value = event.target.textContent;
-//     event.target.style.color = "red";
-//     event.target.style.cursor = "pointer";
-//   }
-// });
-
-// inputFruits.addEventListener(focus, (event) => {
-//   event.target.style.borderColor = "red";
-// });
-// const text = document.querySelector("p");
-// text.innerHTML = "<p>HTML,CSS, <strong>JavaScript</strong></p>";
-
-const categories = document.querySelectorAll("#categories .item");
-const item = document.querySelectorAll(".item");
-console.log(`Numbers of category: ${categories.length}`);
-categories.forEach((element) => {
-  const title = element.querySelector("h2").textContent;
-  const elementsCount = element.querySelectorAll("ul li").length;
-
-  console.log(`Category: ${title}`);
-  console.log(`Elements: ${elementsCount}`);
+btnTitle.addEventListener("click", () => {
+  if (inputTitle.value.trim() !== "") {
+    title.textContent = inputTitle.value;
+  } else {
+    alert("Введіть текст");
+  }
 });
 
-const images = [
-  {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
-  },
-  {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  },
-  {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    alt: "Alpine Spring Meadows",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    alt: "Nature Landscape",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    alt: "Lighthouse Coast Sea",
-  },
-];
+btnColor.addEventListener("click", () => {
+  if (inputColor.value.trim() !== "") {
+    title.style.color = inputColor.value;
+  } else {
+    alert("Введіть колір");
+  }
+});
+
+inputTitle.addEventListener("input", () => {
+  title.textContent = inputTitle.value;
+});
+
+inputColor.addEventListener("input", () => {
+  title.style.color = inputColor.value;
+});
+
+const fruits = document.querySelector("#fruitList");
+const inputFruits = document.querySelector(".input");
+
+fruits.addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    inputFruits.value = event.target.textContent;
+    event.target.style.color = "red";
+    event.target.style.cursor = "pointer";
+  }
+});
+
+inputFruits.addEventListener(focus, (event) => {
+  event.target.style.borderColor = "red";
+});
+const text = document.querySelector("p");
+text.innerHTML = "<p>HTML,CSS, <strong>JavaScript</strong></p>";
+
+// const categories = document.querySelectorAll("#categories .item");
+// const item = document.querySelectorAll(".item");
+// console.log(`Numbers of category: ${categories.length}`);
+// categories.forEach((element) => {
+//   const title = element.querySelector("h2").textContent;
+//   const elementsCount = element.querySelectorAll("ul li").length;
+
+//   console.log(`Category: ${title}`);
+//   console.log(`Elements: ${elementsCount}`);
+// });
+
+// const images = [
+//   {
+//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+//     alt: "Alpine Spring Meadows",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+//     alt: "Nature Landscape",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+//     alt: "Lighthouse Coast Sea",
+//   },
+// ];
+
+// const gallery = document.querySelector(".gallery");
+// const marcup = images
+//   .map(({ url, alt }) => {
+//     return `<li class = "gallery-item"><img src="${url}" alt="${alt}"></li>`;
+//   })
+//   .join("");
+
+// gallery.insertAdjacentHTML("beforeend", marcup);
+
+const book = {
+  title: "JS для початківців",
+  author: "Іван Петренко",
+  year: 2024,
+};
+
+function foo({ title, year }) {
+  console.log(`${title} ${year}`);
+}
+console.log(foo(book));
