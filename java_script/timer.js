beebdc82b8ffafa8f9634ecb32eca239d8a5c3e3
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-08-01 22:52:00 +0300

    js_training

--- java_script/timer.js
+++ java_script/timer.js
@@ -3,26 +3,48 @@ const stopBtn = document.querySelector("button[data-action-stop]");
 const clockface = document.querySelector(".clockface");
 
 class Timer {
+  constructor({ onTick }) {
+    this.onTick = onTick;
+    this.isActive = false;
+  }
+
   start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
     const startTime = Date.now();
 
     setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = currentTime - startTime;
+
       const time = this.getTimeComponent(deltaTime);
 
-      console.log(time);
+      this.onTick(time);
     }, 1000);
   }
 
   getTimeComponent(time) {
-    const hours = (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
-    const mins = time(1000 * 60 * 60) / (1000 * 60);
-    const secs = (time % (1000 * 60)) / 1000;
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
     return { hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
   }
 }
 
-const time = new Timer();
+const time = new Timer({ onTick: updateClockface });
 
-startBtn.addEventListener("click", time.start);
+startBtn.addEventListener("click", time.start.bind(time));
+
+function updateClockface({ hours, mins, secs }) {
+  clockface.textContent = `${hours}:${mins}:${secs}`;
+}
